57a301d33528d9a5915080777b15b83ab5985669
ederpbj <ederpbj@yahoo.com.br>
2020-03-30 20:37:31 -0300

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-default.git)

---
+++ src/pages/contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => (
+    const text =  'meu texto'
+
+    return(
+        <>
+            <h1>About</h1>
+            <h2>{text}</h2>
+        </>
+    )
+)
+
+export default About
