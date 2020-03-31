45602f114d16d13b9792de5ccfefccd7759812e9
ederpbj <ederpbj@yahoo.com.br>
2020-03-30 21:49:12 -0300

    Gatsby JS - Introdução ao componente Layout #04

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -18,7 +18,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
+    Header
       <main>{children}</main>
+      Footer
     </>
   )
 }
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-    import Menu from '../components/Menu';
+import Menu from '../components/Menu';
+import Layout from '../components/Layout';
 
 const Contact = () => {
         const text =  'meu texto Contact';
 
         return(
-            <>
+            <Layout>
                 <Menu />
                 <h1>Contact</h1>
                 <h2>{text}</h2>
-            </>
+            </Layout>
         )
     }
 
